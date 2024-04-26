c8fbbe647a82ad57b944fa7efe6230a29919c723
JcTheSavior <jacobconnell3@gmail.com>
2024-04-26 01:00:36 -0400

    Add starting snake and starting apple
    Snake will always start from the same place, while apple will change for
    each game start

--- app.js
+++ app.js
@@ -5,21 +5,33 @@ window.addEventListener('keydown', event => {
     if(arrowKeys.includes(pressedKey)){
         console.log(pressedKey)
         event.preventDefault()
+        return pressedKey
     }
-    return pressedKey
 })
+// console.log(pressedKey)
 
 // Create grid for game 
 const generateGrid = () => {
     let gridItself = document.querySelector('#gridItself');
-    let startButton = document.querySelector('.startGame');
     gridItself.innerHTML = "";
-    console.log("hello")
+    
+    
     for (let i = 0; i < 625; i++) {
         let gridEach = document.createElement('div')
         gridEach.setAttribute('class', `gridEach${i}`)
         gridItself.append(gridEach)
     }
+    let startSnake = document.querySelector(".gridEach305")
+    let startApple = document.querySelector(`.gridEach${Math.floor(Math.random() * 626)}`)
+    startApple.setAttribute('class', 'gridEachApple')
+    startSnake.setAttribute('class', `gridEachSnake`)
+    let startButton = document.querySelector('.startGame');
     startButton.innerHTML = "Restart Game"
 }
 
+
+//Controlling Snake control
+
+const controlSnake = (e) => {
+    start
+}
